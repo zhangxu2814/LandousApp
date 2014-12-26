4f03dc42c258f9e1abd3eabe9dec6863df567433
张旭 <506089678@qq.com>
2014-12-26 17:08:29 +0800

    beeframework

---
+++ landous/src/com/BeeFramework/view/MyDialog.java
@@ -0,0 +1,46 @@
+package com.BeeFramework.view;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.zykj.landous.R;
+
+public class MyDialog {
+
+	public Dialog mDialog;
+	public TextView dialog_title;
+	public TextView dialog_message;
+	public TextView positive;
+	public TextView negative;
+
+	public MyDialog(Context context, String title, String message) {
+
+		LayoutInflater inflater = LayoutInflater.from(context);
+		View view = inflater.inflate(R.layout.dialog_layout, null);
+
+		mDialog = new Dialog(context, R.style.dialog);
+		mDialog.setContentView(view);
+		mDialog.setCanceledOnTouchOutside(true);
+		
+		dialog_title = (TextView) view.findViewById(R.id.dialog_title);
+		dialog_message = (TextView) view.findViewById(R.id.dialog_message);
+		dialog_title.setText(title);
+		dialog_message.setText(message);
+		
+		positive = (TextView) view.findViewById(R.id.yes);
+		negative = (TextView) view.findViewById(R.id.no);
+		
+	}
+	
+	public void show() {
+		mDialog.show();
+	}
+	
+	public void dismiss() {
+		mDialog.dismiss();
+	}
+
+}
---
+++ landous/src/com/zykj/landous/activity/B1_GoodListActivity.java
@@ -0,0 +1,76 @@
+package com.zykj.landous.activity;
+
+import com.zykj.landous.R;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+
+public class B1_GoodListActivity extends BaseActivity implements
+		OnClickListener {
+	private LinearLayout ll_tab1, ll_tab2, ll_tab3, ll_tab4;
+	private LinearLayout[] tabs = null;
+	Handler mHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			// TODO Auto-generated method stub
+			super.handleMessage(msg);
+			tabs[msg.what].setBackgroundColor(Color.WHITE);
+		}
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.b1_listofgoods);
+		init();
+	}
+
+	private void init() {
+		ll_tab1 = (LinearLayout) findViewById(R.id.ll_tab1);
+		ll_tab1.setOnClickListener(this);
+		ll_tab2 = (LinearLayout) findViewById(R.id.ll_tab2);
+		ll_tab2.setOnClickListener(this);
+		ll_tab3 = (LinearLayout) findViewById(R.id.ll_tab3);
+		ll_tab3.setOnClickListener(this);
+		ll_tab4 = (LinearLayout) findViewById(R.id.ll_tab4);
+		ll_tab4.setOnClickListener(this);
+		tabs = new LinearLayout[] { ll_tab1, ll_tab2, ll_tab3, ll_tab4 };
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.ll_tab1:
+			setBg(0);
+			break;
+		case R.id.ll_tab2:
+			setBg(1);
+			break;
+		case R.id.ll_tab3:
+			setBg(2);
+			break;
+		case R.id.ll_tab4:
+			setBg(3);
+			break;
+
+		}
+
+	}
+
+	private void setBg(int p) {
+		tabs[p].setBackgroundColor(Color.RED);
+		for (int i = 0; i <= 3; i++) {
+			if (i != p) {
+				Message message = new Message();
+				message.what = i;
+				mHandler.sendMessage(message);
+			}
+		}
+	}
+
+}
